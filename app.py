179444ad7c9ac04e06d77c65472b922236b6202c
G. Sai kiran Reddy <47697117+saikiran1828@users.noreply.github.com>
2023-09-07 12:24:25 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,29 +1,6 @@
-# app.py
-
-from flask import Flask, jsonify
-import psycopg2
-from psycopg2 import OperationalError
-
+from flask import Flask
 app = Flask(__name__)
 
-# PostgreSQL database configuration
-db_config = {
-    'dbname': 'sustainability_db_dev_postgsql',
-    'user': 'sustainability_ey_rwuser',
-    'password': 'EY@RW123#',
-    'host': '10.17.33.134',
-    'port': '5432'
-}
-
-@app.route('/test-db')
-def test_db_connection():
-    try:
-        # Attempt to connect to the PostgreSQL database
-        conn = psycopg2.connect(**db_config)
-        conn.close()
-        return jsonify({"message": "Database connection successful!"}), 200
-    except OperationalError as e:
-        return jsonify({"message": "Database connection error: " + str(e)}), 500
-
-if __name__ == '__main__':
-    app.run(debug=True)
+@app.route("/")
+def hello():
+    return "Hello, World!"
